b5b4926605b9b997ddf0f111fa47f3cd197511d4
hsy-05 <107b11602@mailst.cjcu.edu.tw>
2021-01-12 01:48:09 +0800

    會員確認按鈕

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InspiringController;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Subject;      //////////////////
-
+use App\Http\Controllers\UserController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,60 +19,18 @@ use App\Models\Subject;      //////////////////
 */
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('firstHome');
 
-Route::get('/w_co', 'App\Http\Controllers\HomeController@index4')->name('wo_co');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index2'])->name('home');
 
 Route::get('/admin', [App\Http\Controllers\HomeController::class, 'index3'])->name('new');
-// Route::get('/admin', 'App\Http\Controllers\PostController@index')->name('new');
+
+Route::get('/w_co', 'App\Http\Controllers\HomeController@index4')->name('wo_co');
 
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function() {
     Route::resource('posts', 'App\Http\Controllers\PostController');   ////////要打全名
     Route::resource('products', 'App\Http\Controllers\ProductsController');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index2'])->name('home');
-
 
 Route::get('/cart', 'CartController@index')->name('cart.index');
 
-Route::get('/inspire', 'App\Http\Controllers\InspiringController@inspire');
-
- Route::get('/sub1', function(){
-
-    $post = new App\Models\Subject;
-     $post->name = 'computer';
-     $post->save();
-     return $post;
-
- });
-
- Route::get('/sub2', function(){
-
-    $post = new App\Models\Subject;
-     $post->name = 'network';
-     $post->save();
-     return $post;
-
- });
-
- Route::get('/get1', function(){
-
-    $subject = Subject::find(1);
-    $posts = $subject->posts;
-    return $posts;
-
-});
 Auth::routes();
-
-Route::get('/check', function(){
-
-    var_dump(Auth::check());
-
-});
-
-Route::get('/user', function(){
-
-    echo Auth::user();
-
-});
-
-
